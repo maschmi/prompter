d9e59cbc537fa2879b017eb5b4525a9df2c7d33f
Martin Schmidt <inw@immernurwollen.de>
2020-10-04 18:35:28 +0200

    added parsing of text files

--- src/fileloader.rs
+++ src/fileloader.rs
@@ -1,32 +1,91 @@
 use std::{fs, io};
-use std::net::Shutdown::Read;
 use std::io::Error;
-use std::fs::{ReadDir, DirEntry};
+use std::fs::{DirEntry};
 use std::ffi::OsString;
+use std::collections::HashMap;
 
-pub fn get_mp3_files(directory: &str) -> Result<Vec<OsString>, io::Error> {
-    let mut files = read_dir_and_filter(directory, "mp3")?;
-    files.sort();
-    Ok(files)
+
+pub struct FilePairs {
+    pub audio: Option<OsString>,
+    pub text: Option<String>
 }
 
-fn read_dir_and_filter(directory: &str, ending: &str) -> Result<Vec<OsString>, io::Error> {
-    fs::read_dir(directory)?
-        .filter(|res| file_name_ends_on(res, ending))
-        .filter_map(|entry| extract_path(entry))
-        .map(|path| Ok(path))
-        .collect::<Result<Vec<_>, io::Error>>()
+pub struct PlaybackFiles {
+    counter: usize,
+    file_pairs: Vec<FilePairs>,
+    dir_to_parse: String
 }
 
-fn extract_path(entry: Result<DirEntry, Error>) -> Option<OsString> {
-    match entry {
-        Ok(dir_entry) => Some(dir_entry.path().into_os_string()),
-        Err(e) => {
-            println!("Error getting path: {}", e.to_string());
-            None }
+impl PlaybackFiles {
+    pub fn new(dir: &str) -> Self {
+        Self {
+            counter: 0,
+            dir_to_parse: dir.to_string(),
+            file_pairs: Vec::new()
+        }
+    }
+
+    pub fn load(&mut self) -> () {
+        let mp3_files = self.get_mp3_files().unwrap();
+        let txt_files= self.get_text_files().unwrap();
+        let text = PlaybackFiles::parse_text_files(txt_files);
+        let text_values = text.iter().map(|f| f.1).collect::<Vec<&String>>();
+        self.file_pairs = mp3_files.iter().zip(text_values.iter())
+            .map(|z| FilePairs {
+                audio: Some(z.0.to_os_string()),
+                text: Some(z.1.to_string())
+            })
+            .collect::<Vec<FilePairs>>();
+    }
+
+    pub fn next(&mut self) -> &FilePairs {
+        let pair = &self.file_pairs[self.counter];
+        self.counter += 1;
+        pair
+    }
+
+    fn get_mp3_files(&mut self) -> Result<Vec<OsString>, io::Error> {
+        let mut files = self.read_dir_and_filter("mp3")?;
+        files.sort();
+        Ok(files)
+    }
+
+    fn get_text_files(&mut self) -> Result<Vec<OsString>, io::Error> {
+        let mut files = self.read_dir_and_filter("txt")?;
+        files.sort();
+        Ok(files)
+    }
+
+    fn read_dir_and_filter(&mut self, ending: &str) -> Result<Vec<OsString>, io::Error> {
+        fs::read_dir(self.dir_to_parse.clone())?
+            .filter(|res| PlaybackFiles::file_name_ends_on(res, ending))
+            .filter_map(|entry| PlaybackFiles::extract_path(entry))
+            .map(|path| Ok(path))
+            .collect::<Result<Vec<_>, io::Error>>()
+    }
+
+    fn extract_path(entry: Result<DirEntry, Error>) -> Option<OsString> {
+        match entry {
+            Ok(dir_entry) => Some(dir_entry.path().into_os_string()),
+            Err(e) => {
+                println!("Error getting path: {}", e.to_string());
+                None
+            }
+        }
+    }
+
+    fn file_name_ends_on(res: &Result<DirEntry, Error>, ending: &str) -> bool {
+        res.as_ref().unwrap().file_name().to_str().unwrap().ends_with(ending)
+    }
+
+    fn parse_text_files(files: Vec<OsString>) -> HashMap<OsString, String>{
+        let mut map = HashMap::new();
+        files
+            .iter()
+            .for_each(
+                |f| { map.insert(f.to_os_string(), fs::read_to_string(f).unwrap()); }
+            );
+        map
     }
 }
 
-fn file_name_ends_on(res: &Result<DirEntry, Error>, ending: &str) -> bool {
-    res.as_ref().unwrap().file_name().to_str().unwrap().ends_with(ending)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::player::Control;
-use std::{fs, io, time};
+use std::{io, time};
 
 mod fileloader;
 mod player;
@@ -7,20 +7,21 @@ mod player;
 fn main()  -> io::Result<()> {
     let mut audio_playback = player::Player::new("/usr/bin/mpg123", Some("-Tq"));
     let dir_to_search ="/home/martin/Documents";
-    let entries = fileloader::get_mp3_files(dir_to_search).expect("Couĺd not open directory!");
-
-    for x in entries {
-        match audio_playback.play(x.to_str().unwrap()) {
-            Ok(song) => println!("Playing {}", song),
-            Err(e) => panic!(e.to_string())
-        };
-
-        let pause = time::Duration::from_millis(10000);
-        std::thread::sleep(pause);
-        match audio_playback.stop(x.to_str().unwrap()) {
-            Ok(f) => println!("{} was stopped", f),
-            Err(e) => panic!(e.to_string())
-        };
-    }
+    let mut loader = fileloader::PlaybackFiles::new(dir_to_search);
+    loader.load();
+    let current_files = loader.next();
+    let current_audio_file = current_files.audio.as_ref().unwrap().to_str().unwrap();
+    let current_text = current_files.text.as_ref().unwrap();
+    match audio_playback.play(current_audio_file) {
+        Ok(song) => println!("Playing {}", song),
+        Err(e) => panic!(e.to_string())
+    };
+    println!("Text: {}", current_text);
+    let pause = time::Duration::from_millis(10000);
+    std::thread::sleep(pause);
+    match audio_playback.stop(current_audio_file) {
+        Ok(f) => println!("{} was stopped", f),
+        Err(e) => panic!(e.to_string())
+    };
     Ok(())
 }
