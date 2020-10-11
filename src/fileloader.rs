9be69bf5928b9766eea16717ecc352562348bfd2
Martin Schmidt <inw@immernurwollen.de>
2020-10-11 11:54:48 +0200

    adds support for file numbers and missing files

--- src/fileloader.rs
+++ src/fileloader.rs
@@ -4,15 +4,19 @@ use std::fs::{DirEntry};
 use std::ffi::OsString;
 use std::collections::HashMap;
 
-
-pub struct FilePairs {
+pub struct TextAndAudioPair {
     pub audio: Option<OsString>,
     pub text: Option<String>
 }
 
+struct OrderedFiles {
+    order_number: u32,
+    path: OsString
+}
+
 pub struct PlaybackFiles {
     counter: usize,
-    file_pairs: Vec<FilePairs>,
+    audio_text: Vec<TextAndAudioPair>,
     dir_to_parse: String
 }
 
@@ -21,42 +25,53 @@ impl PlaybackFiles {
         Self {
             counter: 0,
             dir_to_parse: dir.to_string(),
-            file_pairs: Vec::new()
+            audio_text: Vec::new()
         }
     }
 
     pub fn load(&mut self) -> () {
         let mp3_files = self.get_mp3_files().unwrap();
-        let txt_files= self.get_text_files().unwrap();
-        let text = PlaybackFiles::parse_text_files(txt_files);
-        let text_values = text.iter().map(|f| f.1).collect::<Vec<&String>>();
-        self.file_pairs = mp3_files.iter().zip(text_values.iter())
-            .map(|z| FilePairs {
-                audio: Some(z.0.to_os_string()),
-                text: Some(z.1.to_string())
-            })
-            .collect::<Vec<FilePairs>>();
+        let txt_files = self.get_text_files().unwrap();
+
+        let text_map= PlaybackFiles::parse_text_files(txt_files);
+        let mut audio_map: HashMap<u32, OsString> = HashMap::new();
+        mp3_files.iter()
+            .for_each(|e| { audio_map.insert(e.order_number, e.path.to_os_string()); });
+
+
+        let mut mp3_files_order = mp3_files.iter().map(|f| f.order_number).collect::<Vec<u32>>();
+
+        let mut order_numbers = text_map.iter().map(|t| t.0.clone()).collect::<Vec<u32>>();
+        order_numbers.append(&mut mp3_files_order);
+        order_numbers.sort();
+        order_numbers.dedup();
+
+        self.audio_text = order_numbers.iter().map(|on| TextAndAudioPair {
+            text: text_map.get(on).cloned(),
+            audio: audio_map.get(on).cloned()
+        }).collect();
     }
 
-    pub fn next(&mut self) -> &FilePairs {
-        let pair = &self.file_pairs[self.counter];
+    pub fn next(&mut self) -> Option<&TextAndAudioPair> {
+        if self.counter >= self.audio_text.len() {
+            return None;
+        }
+        let pair = &self.audio_text[self.counter];
         self.counter += 1;
-        pair
+        Some(pair)
     }
 
-    fn get_mp3_files(&mut self) -> Result<Vec<OsString>, io::Error> {
-        let mut files = self.read_dir_and_filter("mp3")?;
-        files.sort();
+    fn get_mp3_files(&mut self) -> Result<Vec<OrderedFiles>, io::Error> {
+        let files = self.read_dir_and_filter("mp3")?;
         Ok(files)
     }
 
-    fn get_text_files(&mut self) -> Result<Vec<OsString>, io::Error> {
-        let mut files = self.read_dir_and_filter("txt")?;
-        files.sort();
+    fn get_text_files(&mut self) -> Result<Vec<OrderedFiles>, io::Error> {
+        let files = self.read_dir_and_filter("txt")?;
         Ok(files)
     }
 
-    fn read_dir_and_filter(&mut self, ending: &str) -> Result<Vec<OsString>, io::Error> {
+    fn read_dir_and_filter(&mut self, ending: &str) -> Result<Vec<OrderedFiles>, io::Error> {
         fs::read_dir(self.dir_to_parse.clone())?
             .filter(|res| PlaybackFiles::file_name_ends_on(res, ending))
             .filter_map(|entry| PlaybackFiles::extract_path(entry))
@@ -64,9 +79,12 @@ impl PlaybackFiles {
             .collect::<Result<Vec<_>, io::Error>>()
     }
 
-    fn extract_path(entry: Result<DirEntry, Error>) -> Option<OsString> {
+    fn extract_path(entry: Result<DirEntry, Error>) -> Option<OrderedFiles> {
         match entry {
-            Ok(dir_entry) => Some(dir_entry.path().into_os_string()),
+            Ok(dir_entry) => Some(OrderedFiles {
+                order_number: PlaybackFiles::extract_order_number(dir_entry.file_name()),
+                path: dir_entry.path().into_os_string() }
+            ),
             Err(e) => {
                 println!("Error getting path: {}", e.to_string());
                 None
@@ -74,16 +92,25 @@ impl PlaybackFiles {
         }
     }
 
+    fn extract_order_number(filename: OsString) -> u32 {
+        let name = filename.to_str().unwrap();
+        let parts = name.split(" - ").collect::<Vec<_>>();
+        match parts[0].parse(){
+            Ok(o) => o,
+            Err(_) => panic!("Unsupported filename detected {}. Files need to begin with 000 - ", name)
+        }
+    }
+
     fn file_name_ends_on(res: &Result<DirEntry, Error>, ending: &str) -> bool {
         res.as_ref().unwrap().file_name().to_str().unwrap().ends_with(ending)
     }
 
-    fn parse_text_files(files: Vec<OsString>) -> HashMap<OsString, String>{
-        let mut map = HashMap::new();
+    fn parse_text_files(files: Vec<OrderedFiles>) -> HashMap<u32, String>{
+        let mut map: HashMap<u32, String> = HashMap::new();
         files
             .iter()
             .for_each(
-                |f| { map.insert(f.to_os_string(), fs::read_to_string(f).unwrap()); }
+                |f| { map.insert(f.order_number, fs::read_to_string(f.path.clone()).unwrap()); }
             );
         map
     }
--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,35 @@ mod player;
 fn main()  -> io::Result<()> {
     let mut audio_playback = player::Player::new("/usr/bin/mpg123", Some("-Tq"));
     let dir_to_search ="/home/martin/Documents";
+
     let mut loader = fileloader::PlaybackFiles::new(dir_to_search);
     loader.load();
-    let current_files = loader.next();
-    let current_audio_file = current_files.audio.as_ref().unwrap().to_str().unwrap();
-    let current_text = current_files.text.as_ref().unwrap();
-    match audio_playback.play(current_audio_file) {
-        Ok(song) => println!("Playing {}", song),
-        Err(e) => panic!(e.to_string())
-    };
-    println!("Text: {}", current_text);
-    let pause = time::Duration::from_millis(10000);
-    std::thread::sleep(pause);
-    match audio_playback.stop(current_audio_file) {
-        Ok(f) => println!("{} was stopped", f),
-        Err(e) => panic!(e.to_string())
-    };
+
+    while let Some(current_files) = loader.next() {
+
+        if let Some(audio_file) =  current_files.audio.as_ref() {
+            match audio_playback.play(audio_file.to_str().unwrap()) {
+                Ok(song) => println!("Playing {}", song),
+                Err(e) => panic!(e.to_string())
+            };
+        }
+
+        if let Some(text) = current_files.text.as_ref() {
+            println!("Text: {}", text);
+        } else {
+            println!("No text available");
+        }
+
+
+        let pause = time::Duration::from_millis(5000);
+
+        std::thread::sleep(pause);
+        if let Some(audio_file) =  current_files.audio.as_ref() {
+            match audio_playback.stop(audio_file.to_str().unwrap()) {
+                Ok(f) => println!("{} was stopped", f),
+                Err(e) => panic!(e.to_string())
+            };
+        }
+    }
     Ok(())
 }
