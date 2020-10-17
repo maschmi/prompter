31bf8c6cc577715e40f56e5a7d4a9498373a2db9
Martin Schmidt <inw@immernurwollen.de>
2020-10-17 21:19:24 +0200

    uses only one player instance

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use cursive::Cursive;
 use cursive::views::{TextView, OnEventView, TextContentRef};
 use std::borrow::{BorrowMut, Borrow};
 use crate::fileloader::{ PlaybackFiles, TextAndAudioPair};
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::ops::Deref;
 use std::cell::RefCell;
 use cursive::traits::Nameable;
@@ -14,19 +14,18 @@ mod player;
 mod tui;
 
 
-fn update(siv: &mut Cursive, current_files:  PlaybackFiles, audio_player: Player, text: &str) {
+fn update(siv: &mut Cursive, current_files:  PlaybackFiles, audio_player: Weak<RefCell<Player>>, text: &str) {
     let text_view = TextView::new(text).with_name("text");
 
     let new_files = Rc::new(current_files);
-    let new_player = Rc::new(audio_player);
 
     let ref_files_stop = Rc::clone(&new_files);
     let ref_files_play = Rc::clone(&new_files);
     let ref_files_prev = Rc::clone(&new_files);
 
-    let ref_player_stop = Rc::clone(&new_player);
-    let ref_player_play = Rc::clone(&new_player);
-    let ref_player_prev = Rc::clone(&new_player);
+    let ref_player_stop = Weak::clone(&audio_player);
+    let ref_player_play = Weak::clone(&audio_player);
+    let ref_player_prev = Weak::clone(&audio_player);
 
     let event_view = OnEventView::new(text_view)
         .on_event('q', |s| s.quit())
@@ -38,71 +37,92 @@ fn update(siv: &mut Cursive, current_files:  PlaybackFiles, audio_player: Player
     siv.add_layer(event_view);
 }
 
-fn stop_player(siv: &mut Cursive, file_list_ref: Rc<PlaybackFiles>, audio_player_ref: Rc<Player>) {
-    let audio_player = audio_player_ref.deref();
+fn stop_player(siv: &mut Cursive, file_list_ref: Rc<PlaybackFiles>, audio_player_ref: Weak<RefCell<Player>>) {
+    let strong_ref = audio_player_ref.upgrade().unwrap();
+    let audio_player = strong_ref.deref();
     let file_list = file_list_ref.deref().copy();
 
-    let stopped_player = audio_player.stop().unwrap();
+    if let Some(file_to_stop) = file_list.current_files.borrow().as_ref() {
+        if let Some(fp) = file_to_stop.audio.as_ref() {
+            audio_player.deref().borrow_mut().stop(fp.to_str().unwrap());
+        }
+    }
+
     let text = siv.call_on_name("text", |v: &mut TextView|  { v.get_content() });
     siv.pop_layer();
     match text {
-        Some(t) => update(siv.borrow_mut(), file_list, stopped_player, t.source()),
-        _ => update(siv.borrow_mut(), file_list, stopped_player, "Kein Text verfügbar!")
+        Some(t) => update(siv.borrow_mut(), file_list, audio_player_ref, t.source()),
+        _ => update(siv.borrow_mut(), file_list, audio_player_ref, "Kein Text verfügbar!")
     };
 }
 
-fn next(siv: &mut Cursive, file_list_ref: Rc<PlaybackFiles>, audio_player_ref: Rc<Player>)  {
-    let audio_player = audio_player_ref.deref();
-    let file_list = file_list_ref.deref().copy();
+fn next(siv: &mut Cursive, file_list_ref: Rc<PlaybackFiles>, audio_player_ref: Weak<RefCell<Player>>)  {
+    if let Some(audio_player_upgrade) = audio_player_ref.upgrade() {
+        let strong_ref = audio_player_ref.upgrade().unwrap();
+        let audio_player = strong_ref.deref();
+        let file_list = file_list_ref.deref().copy();
 
-    let stopped_player = stop(file_list.current_files.borrow(), audio_player.copy());
-
-    siv.pop_layer();
-
-    if let Some(next_files) = file_list.next_files.as_ref() {
-        let moved_list = file_list.move_next();
-        let mut text = "Kein Text verfügbar.";
-
-        if let Some(txt) = next_files.text.as_ref() {
-            text = txt;
+        if let Some(file_to_stop) = file_list.current_files.borrow().as_ref() {
+            if let Some(fp) = file_to_stop.audio.as_ref() {
+                audio_player.deref().borrow_mut().stop(fp.to_str().unwrap());
+            }
         }
+        siv.pop_layer();
 
-        if let Some(audio_file) = next_files.audio.as_ref() {
-            let player = audio_player.play(audio_file.to_str().unwrap()).unwrap();
+        if let Some(next_files) = file_list.next_files.as_ref() {
+            let moved_list = file_list.move_next();
+            let mut text = "Kein Text verfügbar.";
+
+            if let Some(txt) = next_files.text.as_ref() {
+                text = txt;
+            }
+
+            if let Some(audio_file) = next_files.audio.as_ref() {
+                audio_player.borrow_mut().play(audio_file.to_str().unwrap());
                 //.expect(panic!("Uh oh"));
-            update(siv.borrow_mut(), moved_list, player, text);
+                update(siv.borrow_mut(), moved_list, audio_player_ref, text);
+            } else {
+                update(siv.borrow_mut(), moved_list, audio_player_ref, text);
+            }
         } else {
-            update(siv.borrow_mut(), moved_list, stopped_player, text);
+            update(siv.borrow_mut(), file_list, audio_player_ref, "Nix mehr da.")
         }
-    } else {
-        update(siv.borrow_mut(), file_list, stopped_player, "Nix mehr da.")
     }
 }
-fn previous(siv: &mut Cursive, file_list_ref: Rc<PlaybackFiles>, audio_player_ref: Rc<Player>)  {
-    let audio_player = audio_player_ref.deref();
-    let file_list = file_list_ref.deref().copy();
+fn previous(siv: &mut Cursive, file_list_ref: Rc<PlaybackFiles>, audio_player_ref: Weak<RefCell<Player>>)  {
+    if let Some(audio_player_upgrade) = audio_player_ref.upgrade() {
+        let strong_ref = audio_player_ref.upgrade().unwrap();
+        let audio_player = strong_ref.deref();
+        let file_list = file_list_ref.deref().copy();
 
-    let stopped_player = stop(file_list.current_files.borrow(), audio_player.copy());
+        let file_to_stop = file_list.current_files.borrow().as_ref().unwrap();
 
-    siv.pop_layer();
-
-    if let Some(next_files) = file_list.previous_files.as_ref() {
-        let moved_list = file_list.move_back();
-        let mut text = "Kein Text verfügbar.";
-
-        if let Some(txt) = next_files.text.as_ref() {
-            text = txt;
+        if let Some(file_to_stop) = file_list.current_files.borrow().as_ref() {
+            if let Some(fp) = file_to_stop.audio.as_ref() {
+                audio_player.deref().borrow_mut().stop(fp.to_str().unwrap());
+            }
         }
 
-        if let Some(audio_file) = next_files.audio.as_ref() {
-            let player = audio_player.play(audio_file.to_str().unwrap()).unwrap();
-            //.expect(panic!("Uh oh"));
-            update(siv.borrow_mut(), moved_list, player, text);
+        siv.pop_layer();
+
+        if let Some(prev_files) = file_list.previous_files.as_ref() {
+            let moved_list = file_list.move_back();
+            let mut text = "Kein Text verfügbar.";
+
+            if let Some(txt) = prev_files.text.as_ref() {
+                text = txt;
+            }
+
+            if let Some(audio_file) = prev_files.audio.as_ref() {
+                audio_player.borrow_mut().play(audio_file.to_str().unwrap().borrow());
+                //.expect(panic!("Uh oh"));
+                update(siv.borrow_mut(), moved_list, audio_player_ref, text);
+            } else {
+                update(siv.borrow_mut(), moved_list, audio_player_ref, text);
+            }
         } else {
-            update(siv.borrow_mut(), moved_list, stopped_player, text);
+            update(siv.borrow_mut(), file_list, audio_player_ref, "Am Anfang war nix.")
         }
-    } else {
-        update(siv.borrow_mut(), file_list, stopped_player, "Nix mehr da.")
     }
 }
 
@@ -114,24 +134,17 @@ fn update_text_view(siv: &mut Cursive, text_to_display: &str) {
                      });
 }
 
-fn stop(file_to_stop: &Option<TextAndAudioPair>, audio_player: Player) -> Player {
-    if let Some(current_files) = file_to_stop {
-        if let Some(_) = &current_files.audio {
-            return audio_player.stop().unwrap();
-        }
-    }
-    audio_player.empty().unwrap()
-}
 
 fn main()  -> io::Result<()> {
 
     let dir_to_search ="/home/martin/Documents";
     let initital_files = PlaybackFiles::initialize(dir_to_search);
 
-    let audio_player = player::Player::new("/usr/bin/mpg123", Some("-q"));
+    let mut audio_player = player::Player::new("/usr/bin/mpg123", Some("-q"));
+    let audio_player_ref = Rc::new(RefCell::new(audio_player));
 
     let mut siv = cursive::default();
-    update(siv.borrow_mut(), initital_files, audio_player, "Welcome to prompter.");
+    update(siv.borrow_mut(), initital_files, Rc::downgrade(&audio_player_ref), "Welcome to prompter.");
     siv.run();
 
     Ok(())
--- src/player.rs
+++ src/player.rs
@@ -1,22 +1,16 @@
 use std::process::Child;
 use std::process::Command;
 use std::collections::HashMap;
-use std::rc::{Rc, Weak};
-use std::cell::RefCell;
 use std::borrow::BorrowMut;
-use std::ops::Deref;
 
 
 pub struct Player {
     playback_program: String,
     std_args: String,
-    sound_process: Option<Rc<RefCell<Child>>>,
-    copied_process: Option<Weak<RefCell<Child>>>,
-    playing_file: Option<String>
+    sound_process: HashMap<String, Child>
 }
 
 impl Player {
-
     pub fn new(program_path: &str, default_args: Option<&str>) -> Self {
         Self {
             playback_program: program_path.to_string(),
@@ -24,74 +18,32 @@ impl Player {
                 Some(arg) => arg.to_string(),
                 None => "".to_string()
             },
-            sound_process: None,
-            copied_process: None,
-            playing_file: None
-        }
-    }
-
-    pub fn copy(&self) -> Player {
-        let mut copied_process = None;
-        if let Some(parent_proc) = &self.sound_process.as_ref() {
-            copied_process = Some(Rc::downgrade(parent_proc));
-        }
-
-        Player {
-            playback_program: String::from(&self.playback_program),
-            sound_process: None,
-            copied_process: copied_process,
-            std_args: String::from(&self.std_args),
-            playing_file: self.playing_file.as_ref().map(|s| String::from(s))
+            sound_process: HashMap::new()
         }
     }
 }
 
 impl Control for Player {
 
-    fn play(&self, filepath: &str) -> Result<Player, std::io::Error> {
+    fn play(&mut self, filepath: &str) -> Result<String, std::io::Error> {
         let child_process = Command::new(&self.playback_program)
             .args(&[&self.std_args, filepath.clone()])
             .spawn()?;
-        Ok(Player{
-            playback_program: String::from(&self.playback_program),
-            std_args: String::from(&self.std_args),
-            playing_file: Some(String::from(filepath)),
-            sound_process: Some(Rc::new(RefCell::new(child_process))),
-            copied_process: None
-        })
+        self.sound_process.insert(filepath.to_string(), child_process);
+        Ok(filepath.to_string())
     }
 
-    fn stop(&self) -> Result<Player, std::io::Error> {
-        if let Some(owner) = self.sound_process.as_ref() {
-            owner.deref().borrow_mut().kill();
-        } else if let Some(copy) = self.copied_process.as_ref() {
-            copy.upgrade().unwrap().deref().borrow_mut().kill();
+    fn stop(&mut self, filepath: &str) -> Result<String, std::io::Error> {
+        if let Some(mut child) = self.sound_process.remove(filepath) {
+            child.kill();
         }
 
-        Ok(Player {
-            playback_program: String::from(&self.playback_program),
-            std_args: String::from(&self.std_args),
-            playing_file: None,
-            sound_process: None,
-            copied_process: None
-        })
+        Ok(filepath.to_string())
     }
-
-    fn empty(&self) -> Result<Player, std::io::Error> {
-        Ok(Player {
-            playback_program: String::from(&self.playback_program),
-            std_args: String::from(&self.std_args),
-            playing_file: None,
-            sound_process: None,
-            copied_process: None
-        })
-    }
-
 }
 
 pub trait Control {
-    fn play(&self, filepath: &str) -> Result<Player, std::io::Error>;
-    fn stop(&self) ->  Result<Player, std::io::Error>;
-    fn empty(&self) -> Result<Player, std::io::Error>;
+    fn play(&mut self, filepath: &str) -> Result<String, std::io::Error>;
+    fn stop(&mut self, filepath: &str) ->  Result<String, std::io::Error>;
 }
 
